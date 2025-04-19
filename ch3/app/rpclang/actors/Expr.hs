44c9f8cab2a97eeffc5b1a63cbace80e4bf946f4
chaendaya <jini971@naver.com>
2025-04-19 12:04:55 +0900

    fix pprint

--- ch3/app/rpclang/actors/Expr.hs
+++ ch3/app/rpclang/actors/Expr.hs
@@ -2,7 +2,7 @@
 {-# HLINT ignore "Use camelCase" #-}
 module Actors.Expr(Program,Exp(..),Identifier,UnaryOp(..), pprint) where
 
-import Data.List (intersperse)
+import Data.List (intersperse, intercalate)
 import Data.List (isPrefixOf)
 
 type Program = Exp
@@ -45,38 +45,43 @@ data UnaryOp = IsZero | IsNull | Car | Cdr | Print deriving Show
 
 type Identifier = String
 
-pprint :: Exp -> String
-pprint (Const_Exp n) = show n
-pprint (Diff_Exp e1 e2) = "-" ++ "(" ++ pprint e1 ++ " , " ++ pprint e2 ++ ")"
-pprint (If_Exp e1 e2 e3) = "if " ++ pprint e1 ++ " then " ++ pprint e2 ++ " else " ++ pprint e3
-pprint (Var_Exp id) = id
-pprint (Let_Exp id e1 e2) = "let " ++ id ++ " = " ++ pprint e1 ++ " in " ++ pprint e2
-pprint (Letrec_Exp lst e) =
-  "letrec " ++ concatMap (\(id1, id2, e) -> id1 ++ "(" ++ id2 ++ ") = " ++ pprint e ++ " ") lst ++ pprint e ++ " in " ++ pprint e
-pprint (Proc_Exp id e) = "proc " ++ "(" ++ id ++ ") " ++ pprint e
-pprint (Call_Exp e1 e2) = "(" ++ pprint e1 ++ " " ++ pprint e2 ++ ")"
-pprint (Block_Exp lst) = "begin " ++ concatMap (\e -> pprint e ++ "; ") (init lst) ++ pprint (last lst) ++ " " ++ "end"
-pprint (Set_Exp id e) = "set " ++ id ++ " = " ++ pprint e
-pprint (Const_List_Exp lst) = "[" ++ concatMap (\n -> show n ++ ", ") lst ++ "]"
--- pprint (Unary_Exp op e) = show op ++ "(" ++ pprint e ++ ")"
-pprint (Unary_Exp op e) = 
-  case op of
-    IsZero -> "zero? (" ++ pprint e ++ ")"
-    IsNull -> "null? (" ++ pprint e ++ ")"
-    Car    -> "car (" ++ pprint e ++ ")"
-    Cdr    -> "cdr (" ++ pprint e ++ ")"
-    Print  -> "print (" ++ pprint e ++ ")"
+-- pprint :: Exp -> String
+-- pprint (Const_Exp n) = show n
+-- pprint (Diff_Exp e1 e2) = "-" ++ "(" ++ pprint e1 ++ " , " ++ pprint e2 ++ ")"
+-- pprint (If_Exp e1 e2 e3) = "if " ++ pprint e1 ++ "\nthen " ++ pprint e2 ++ "\nelse " ++ pprint e3
+-- pprint (Var_Exp id) = id
+-- pprint (Let_Exp id e1 e2) = "\nlet " ++ id ++ " = " ++ pprint e1 ++ " in\n" ++ pprint e2
+-- pprint (Letrec_Exp bindings body) =
+--   "letrec\n" ++ unlines (map pprintBinding bindings) ++ "in\n" ++ indent (pprint body)
+--   where
+--     pprintBinding (name, param, expr) =
+--       "  " ++ name ++ " (" ++ param ++ ") = " ++ pprint expr
+--     indent = unlines . map ("  " ++) . lines
+-- -- pprint (Letrec_Exp lst e) =
+-- --   "letrec " ++ concatMap (\(id1, id2, e) -> id1 ++ "(" ++ id2 ++ ") = " ++ pprint e ++ " ") lst ++ pprint e ++ " in " ++ pprint e
+-- pprint (Proc_Exp id e) = "proc " ++ "(" ++ id ++ ") " ++ pprint e
+-- pprint (Call_Exp e1 e2) = "(" ++ pprint e1 ++ " " ++ pprint e2 ++ ")"
+-- pprint (Block_Exp lst) = "begin " ++ concatMap (\e -> pprint e ++ "; ") (init lst) ++ pprint (last lst) ++ " " ++ "end"
+-- pprint (Set_Exp id e) = "set " ++ id ++ " = " ++ pprint e
+-- pprint (Const_List_Exp lst) = "[" ++ concatMap (\n -> show n ++ ", ") lst ++ "]"
 
-pprint (Send_Exp lst) = "send (" ++ concatMap (\e -> pprint e ++ ", ") (init lst) ++ pprint (last lst) ++ ")"
-pprint (Ready_Exp e) = "ready (" ++ pprint e ++ ")"
-pprint (New_Exp e) = "new (" ++ pprint e ++ ")"
-pprint (Eq_Actor_Exp e1 e2) = "actor? (" ++ pprint e1 ++ ", " ++ pprint e2 ++ ")"
+-- pprint (Unary_Exp op e) = 
+--   case op of
+--     IsZero -> "zero? (" ++ pprint e ++ ")"
+--     IsNull -> "null? (" ++ pprint e ++ ")"
+--     Car    -> "car (" ++ pprint e ++ ")"
+--     Cdr    -> "cdr (" ++ pprint e ++ ")"
+--     Print  -> "print (" ++ pprint e ++ ")"
 
-pprint (Tuple_Exp lst) =  "(" ++ concatMap (\e -> pprint e ++ ", ") (init lst) ++ pprint (last lst) ++ ")"
-pprint (LetTuple_Exp lst e1 e2) =  "let (" ++ concatMap (++ ", ") (init lst) ++ (last lst) ++ ") = " ++ pprint e1 ++ " in " ++ pprint e2
+-- pprint (Send_Exp lst) = "send (" ++ concatMap (\e -> pprint e ++ ", ") (init lst) ++ pprint (last lst) ++ ")"
+-- pprint (Ready_Exp e) = "ready (" ++ pprint e ++ ")"
+-- pprint (New_Exp e) = "new (" ++ pprint e ++ ")"
+-- pprint (Eq_Actor_Exp e1 e2) = "actor? (" ++ pprint e1 ++ ", " ++ pprint e2 ++ ")"
+
+-- pprint (Tuple_Exp exprs) = "(" ++ intercalate ", " (map pprint exprs) ++ ")"
+-- pprint (LetTuple_Exp vars e1 e2) = "let (" ++ intercalate ", " vars ++ ") = " ++ pprint e1 ++ " in " ++ pprint e2
 
 -------------------------
--- 
 
 -- pprint :: Exp -> String
 -- pprint = pprintWithIndent 0
@@ -123,3 +128,53 @@ pprint (LetTuple_Exp lst e1 e2) =  "let (" ++ concatMap (++ ", ") (init lst) ++ 
 
 -- indentStr :: Int -> String
 -- indentStr n = replicate n ' '
+
+---
+pprint :: Exp -> String
+pprint = pprint' 0
+
+pprint' :: Int -> Exp -> String
+pprint' indentLevel expr =
+  let ind = replicate indentLevel ' '
+      next = indentLevel + 2
+  in case expr of
+    Const_Exp n -> show n
+    Diff_Exp e1 e2 -> "-" ++ "(" ++ pprint' 0 e1 ++ " , " ++ pprint' 0 e2 ++ ")"
+    If_Exp e1 e2 e3 -> "if " ++ pprint' 0 e1 ++ "\n" ++ ind ++ "then " ++ pprint' next e2 ++ "\n" ++ ind ++ "else " ++ pprint' next e3
+    Var_Exp id -> id
+    Let_Exp id e1 e2 ->
+      ind ++ "let " ++ id ++ " = " ++ pprint' 0 e1 ++ " in\n" ++ pprint' indentLevel e2
+    Letrec_Exp bindings body ->
+      ind ++ "letrec\n" ++ concatMap (pprintBinding next) bindings ++ ind ++ "in\n" ++ pprint' next body
+      where
+        pprintBinding n (name, param, expr) =
+          replicate n ' ' ++ name ++ " (" ++ param ++ ") = " ++ pprint' n expr ++ "\n"
+    Proc_Exp id e -> 
+      ind ++ "proc (" ++ id ++ ")\n" ++ pprint' next e
+    Call_Exp e1 e2 -> 
+      "(" ++ pprint' 0 e1 ++ " " ++ pprint' 0 e2 ++ ")"
+    Block_Exp lst -> 
+      "begin " ++ concatMap ((++ "; ") . pprint' 0) (init lst) ++ pprint' 0 (last lst) ++ " end"
+    Set_Exp id e -> 
+      "set " ++ id ++ " = " ++ pprint' 0 e
+    Const_List_Exp lst -> 
+      "[" ++ intercalate ", " (map show lst) ++ "]"
+    Unary_Exp op e ->
+      case op of
+        IsZero -> "zero? (" ++ pprint' 0 e ++ ")"
+        IsNull -> "null? (" ++ pprint' 0 e ++ ")"
+        Car    -> "car (" ++ pprint' 0 e ++ ")"
+        Cdr    -> "cdr (" ++ pprint' 0 e ++ ")"
+        Print  -> "print (" ++ pprint' 0 e ++ ")"
+    Send_Exp lst -> 
+      "send (" ++ intercalate ", " (map (pprint' 0) lst) ++ ")"
+    Ready_Exp e -> 
+      "ready (" ++ pprint' 0 e ++ ")"
+    New_Exp e -> 
+      "new (" ++ pprint' 0 e ++ ")"
+    Eq_Actor_Exp e1 e2 -> 
+      "actor? (" ++ pprint' 0 e1 ++ ", " ++ pprint' 0 e2 ++ ")"
+    Tuple_Exp exprs -> 
+      "(" ++ intercalate ", " (map (pprint' 0) exprs) ++ ")"
+    LetTuple_Exp vars e1 e2 ->
+      ind ++ "let (" ++ intercalate ", " vars ++ ") = " ++ pprint' 0 e1 ++ " in\n" ++ pprint' indentLevel e2
